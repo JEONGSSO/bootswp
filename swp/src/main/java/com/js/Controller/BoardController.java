a3a3807ccdab29c12594c2f54a3d22027938ca73
jeongsso <rpsha33@naver.com>
2018-10-21 19:53:51 +0900

    jpa 삭제

--- swp/src/main/java/com/js/Controller/BoardController.java
+++ swp/src/main/java/com/js/Controller/BoardController.java
@@ -2,13 +2,13 @@ package com.js.Controller;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import com.js.JPA.Jpa;
 import com.js.domian.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +20,19 @@ public class BoardController {
     @Autowired
     Jpa repository;    //jpaRepository
 
-    @PostMapping("/add")
-    public User name(@RequestBody User user) {
+        @PostMapping("/add")
+    public User addtion(@RequestBody User user) {
         User userData = repository.save(user);
-        
+        System.out.println(user.getId() + " 가입 되었습니다.");
         return userData;
     } 
+    
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable String id) {
+        repository.delete(id);
+        System.out.println(id + " 삭제 되었습니다.");
+    } 
+
 
     @RequestMapping("/list")
     public List<User> list(Model model) {
--- swp/src/main/java/com/js/JPA/Jpa.java
+++ swp/src/main/java/com/js/JPA/Jpa.java
@@ -3,7 +3,13 @@ package com.js.JPA;
 import com.js.domian.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface Jpa extends JpaRepository<User, String> {
-    
-}
+
+    public interface Jpa extends JpaRepository<User, String> {
+
+        @Query("delete from user where id =: id")
+        User delete(String id);d
+    }
